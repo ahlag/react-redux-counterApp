d2254bb2a62e01019e4d5c4f19816ab8b67d7945
Chang Yong Lik <theeahlag@gmail.com>
2022-03-05 17:54:59 +0900

    Refactored project

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import {configureStore, createSlice} from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: { counter: 0 },
+    reducers: {
+        increment: (state, action) => {
+            state.counter += 1;
+        },
+        decrement: (state, action) => {
+            state.counter -= 1;
+        },
+        addBy: (state, action) => { 
+            state.counter += 10;
+        }
+    }
+})
+
+export const actions = counterSlice.actions;
+
+export const store = configureStore({
+    reducer: counterSlice.reducer
+});
